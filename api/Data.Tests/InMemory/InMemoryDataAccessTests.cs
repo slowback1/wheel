c3111eac4aafcb57c37129b6767480c237ef458f
Andrew Wobeck <dwobeck@gmail.com>
2024-10-31 21:42:13 -0500

    retroactively add some unit tests ensuring the correct types are passed into the InMemoryDataAccess class

--- api/Data.Tests/InMemory/InMemoryDataAccessTests.cs
+++ api/Data.Tests/InMemory/InMemoryDataAccessTests.cs
@@ -7,6 +7,8 @@ public class InMemoryDataAccessTests
     [Test]
     [TestCase(nameof(InMemoryDataAccess.WheelCreator), typeof(InMemoryWheelCreator))]
     [TestCase(nameof(InMemoryDataAccess.WheelRetriever), typeof(InMemoryWheelRetriever))]
+    [TestCase(nameof(InMemoryDataAccess.UserCreator), typeof(InMemoryUserCreator))]
+    [TestCase(nameof(InMemoryDataAccess.UserRetriever), typeof(InMemoryUserRetriever))]
     public void Properties_ReturnCorrectTypes(string propertyName, Type expectedType)
     {
         var dataAccess = new InMemoryDataAccess();
