b3ba9a1205a2710081ec0307e86632ec524948a7
Andrew Wobeck <dwobeck@gmail.com>
2024-12-05 18:24:30 -0600

    add a build and push script for the API

---
+++ api/scripts/build-and-push-backend.sh
@@ -0,0 +1,33 @@
+﻿# Usage
+# Env Variables:
+# REGISTRY: the docker registry to push the image to. ex: hub.docker:5000
+# BUILD_NUMBER: the unique to this build number.  ex: 1
+# PROJECT_NAME: The name of the project.  Defaults to "api". ex: my-cool-project-name
+
+cd $(git rev-parse --show-toplevel)/api || exit 1
+
+BUILD_NUMBER=latest
+REGISTRY="hub.docker.com:5000"
+PROJECT_NAME="slowback1/wheel-of-slowback-api"
+
+if [ -z ${REGISTRY} ]; then
+  echo "REGISTRY is unset, please set it before running this script";
+  exit 1
+fi
+
+if [ -z ${BUILD_NUMBER} ]; then
+  echo "BUILD_NUMBER is unset, please set it before running this script";
+  exit 1
+fi
+
+if [ -z ${PROJECT_NAME} ]; then
+  echo "PROJECT_NAME is unset, setting to 'frontend'";
+  PROJECT_NAME="api"
+fi
+
+TAG="${PROJECT_NAME}:${BUILD_NUMBER}"
+
+docker build -t=${TAG} -f WebApi/Dockerfile --target=final .
+docker image tag ${TAG}
+
+docker image push ${TAG}
