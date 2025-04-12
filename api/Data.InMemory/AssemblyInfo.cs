8ded76d3c374529a5a5bb25eafb753d64ab071a1
Andrew Wobeck <dwobeck@gmail.com>
2025-04-12 13:30:46 -0500

    Refactor the Data project into separate projects for each variation of data access.
    Pre-emptively doing this since the Discord bot will only support the File variant for now, and this way it does not need to feign support for the other variations.

--- api/Data.InMemory/AssemblyInfo.cs
+++ api/Data.InMemory/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Data.Tests")]
+[assembly: InternalsVisibleTo("Data.InMemory.Tests")]
 [assembly: InternalsVisibleTo("UseCases.BDD.Tests")]
