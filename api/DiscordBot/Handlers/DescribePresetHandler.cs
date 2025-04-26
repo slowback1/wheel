49a2828aaea209e56ce183737784e7bfd011a87b
Andrew Wobeck <dwobeck@gmail.com>
2025-04-25 22:01:58 -0500

    Add discord bot handler to describe presets

---
+++ api/DiscordBot/Handlers/DescribePresetHandler.cs
@@ -0,0 +1,34 @@
+﻿using Common.Data;
+using Common.Interfaces;
+using DiscordBot.Models;
+using UseCases.Wheel;
+
+namespace DiscordBot.Handlers;
+
+[DiscordAction("describe")]
+public class DescribePresetHandler : BaseDiscordHandler, IDiscordHandler
+{
+    public DescribePresetHandler(IDataAccess dataAccess, DiscordActionContext context) : base(dataAccess, context)
+    {
+    }
+
+    public async Task<string> HandleAsync()
+    {
+        var user = await GetOrCreateUser(Context.UserId);
+
+        var presetName = Context.Argument.Trim();
+        if (string.IsNullOrEmpty(presetName))
+            return "Preset name cannot be empty. Please provide a preset name.";
+
+        var useCase = new GetWheelSettingUseCase(DataAccess);
+        var getResult = await useCase.GetWheelSetting(presetName);
+
+        if (getResult.Status == FeatureResultStatus.Ok)
+        {
+            var slices = string.Join(',', getResult.Data!.Slices.Select(x => x.Label));
+            return slices;
+        }
+
+        return $"Error retrieving preset: {getResult.Exception?.Message ?? "Unknown error"}";
+    }
+}
